944c5123fc53f97479b041e710478247c4c1618e
ramd20 <ramdevakumar20>
2026-04-08 18:12:13 -0400

    Initial Commit

---
+++ Stepper/StepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperApp.swift
+//  Stepper
+//
+//  Created by Ram Devakumar on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct StepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
